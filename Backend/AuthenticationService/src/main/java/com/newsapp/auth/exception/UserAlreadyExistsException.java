453a3b3dfade27e75f30e6fcef8343acec7f3ab6
Mohd Nazim <mohdnazim957@gmail.com>
2024-05-23 15:10:38 +0530

    Authentication Service Postgres

--- Backend/AuthenticationService/src/main/java/com/newsapp/auth/exception/UserAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-//package com.newsapp.auth.exception;
-//
-//import org.springframework.http.HttpStatus;
-//import org.springframework.web.bind.annotation.ResponseStatus;
-//
-//@ResponseStatus(value = HttpStatus.CONFLICT,reason = "Invalid credentials")
-//public class UserAlreadyExistsException extends Throwable {
-//}
--- Backend/AuthenticationService/src/test/java/com/newsapp/auth/util/Role.java
+++
@@ -1,6 +0,0 @@
-package com.newsapp.auth.util;
-
-public enum Role {
-    ADMIN,
-    USER
-}
